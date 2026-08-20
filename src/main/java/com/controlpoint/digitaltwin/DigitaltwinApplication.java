4609726c598370d810d69d3c99547121aedd1d73
MostafaAbdulazziz <es-moustafa.abdelazziz2027@alexu.edu.eg>
2026-08-20 16:43:08 +0300

    init project

---
+++ src/main/java/com/controlpoint/digitaltwin/DigitaltwinApplication.java
@@ -0,0 +1,13 @@
+package com.controlpoint.digitaltwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitaltwinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitaltwinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/controlpoint/digitaltwin/DigitaltwinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controlpoint.digitaltwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitaltwinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
